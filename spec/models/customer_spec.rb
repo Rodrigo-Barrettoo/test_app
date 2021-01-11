4d9b481a2c6a391d9bab6db4e15017287f3db5cd
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-11 10:33:58 -0300

    Começando com Fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  fixtures :all
+  
+  it 'Create a Customer' do
+    customer = customers(:rodrigo)
+    expect(customer.name).to eq('Barretin Kazaper')
+  end
 end

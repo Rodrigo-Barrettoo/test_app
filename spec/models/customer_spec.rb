5a510c54bb5f2a0cbb7b0bb52d15cab9b85ecc07
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-11 11:48:18 -0300

    Sobrescrevendo atributos e Aliases para fábricas

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it '#full_name' do
-    customer = create(:customer)
+    customer = create(:user) # ou create(:customer)
     expect(customer.full_name).to start_with('Sr. ')
   end
+
+  it '#full_name' do
+    customer = create(:customer, name: 'Barretin Kazaper')
+    expect(customer.full_name).to start_with('Sr. Barretin Kazaper')
+  end
+
+  it { expect{create(:customer)}.to change{Customer.all.size}.by(1) }
 end

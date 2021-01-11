3848664d13299ae37a0b857aeeb8090175ac58cc
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-11 12:06:45 -0300

    Herança

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,9 +6,19 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to start_with('Sr. ')
   end
 
-  it '#full_name' do
+  it '#full_name - Sobrescrevendo Atributos' do
     customer = create(:customer, name: 'Barretin Kazaper')
     expect(customer.full_name).to start_with('Sr. Barretin Kazaper')
+  end
+
+  it 'Herança - Vip' do
+    customer = create(:customer_vip)
+    expect(customer.vip).to eq(true)
+  end
+
+  it 'Herança - Default' do
+    customer = create(:customer_default)
+    expect(customer.vip).to eq(false)
   end
 
   it { expect{create(:customer)}.to change{Customer.all.size}.by(1) }

a57cb82af53354cea6886f6266452e7cd1b1dd5b
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-12 12:29:18 -0300

    Create List

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Order, type: :model do
     puts order.customer.name
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it '3 Pedidos juntos' do
+    orders = create_list(:order, 3)
+    puts orders.inspect
+    expect(orders.count).to eq(3)
+  end
 end

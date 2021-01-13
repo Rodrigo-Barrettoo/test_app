701b1957a5e51fa45c764bc83cd662c1d9cd86fc
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-13 09:46:17 -0300

    Modos de Gravação do VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,5 @@
 describe 'HTTParty' do
-  it 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  it 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: {
     #     'content-type': 'application/json'

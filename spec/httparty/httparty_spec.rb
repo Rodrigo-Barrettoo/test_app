8bb09be16bc3634766540309b24b460830ef27d1
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-13 09:42:10 -0300

    VCR com URIs não determinísticas

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,12 +1,12 @@
 describe 'HTTParty' do
-  it 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts'} do
+  it 'HTTParty', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
     # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
     #   to_return(status: 200, body: "", headers: {
     #     'content-type': 'application/json'
     #   })
 
 
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
     content_type = response.headers['content-type']
     # puts content_type
     expect(content_type).to match(/application\/json/)

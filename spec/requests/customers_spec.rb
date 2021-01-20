d5b6e5c41963dc0b970d5b57be9544cf7b0042ab
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-20 10:32:00 -0300

    POST - JSON

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Customers", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "index - JSON 200 ok" do
+    it "index - JSON" do
       get '/customers.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json([
@@ -17,13 +17,33 @@ RSpec.describe "Customers", type: :request do
       ])
     end
 
-    it "show - JSON 200 ok" do
+    it "show - JSON" do
       get '/customers/1.json'
       expect(response).to have_http_status(200)
       expect(response.body).to include_json(
         id: /\d/,
         name: (be_kind_of String),
         email: (be_kind_of String)
+      )
+    end
+
+    it "post - JSON" do
+      member = create(:member)
+      login_as(member, :scope => :member)
+
+      headers = { 'ACCEPT' => 'application/json'}
+
+      customers_params = attributes_for(:customer)
+
+      post '/customers.json', params: {
+        customer: customers_params,
+        headers: headers
+      }
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email) # retorna um erro caso nao encontre
       )
     end
   end
